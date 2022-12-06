9e5b4824d6c26ce7a2a67ada21d3c13134ad6338
Janmejay Chatterjee <70024430+NotTheRightGuy@users.noreply.github.com>
2022-12-06 16:18:36 +0530

    ChatApp HomePage

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -2,16 +2,17 @@ import "../styles/homepage.css";
 function HomePage() {
     return (
         <div className="main-container">
-            <div className="top-banner">
-                <div className="title">Connecting People Together</div>
+            <div className="Homepage-title">CHATT'ER</div>
+            <div className="Homepage-subtitle">For all your private deeds</div>
+            <div className="login-button">
                 <button
-                    className="getStarted"
-                    onClick={() => (window.location.href = "/login")}
+                    onClick={() => {
+                        window.location.href = "/login";
+                    }}
                 >
                     Get Started
                 </button>
             </div>
-            <div className="bottom-container"></div>
         </div>
     );
 }
